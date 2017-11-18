ca248fe4c315e0adfe22e09642cb71ef3aa6e7f6
hzchenwei3 <hzchenwei3@corp.netease.com>
2017-11-18 21:01:21 +0800

    first commit

---
+++ src/main/java/com/cw/web/HelloController.java
@@ -0,0 +1,40 @@
+package com.cw.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by tata on 2017/11/12.
+ */
+@Controller
+//@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+//    @RequestMapping("/say")
+    @PostMapping("/say")
+    public String hello(){
+
+        return "Hello Spring Boot";
+
+    }
+
+    @GetMapping("/books")
+//    @ResponseBody
+//    public String getAll(){
+//
+//        return "books";
+//
+//    }
+    public Object getAll(){
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "hello");
+        map.put("age", 18);
+        return map;
+
+    }
+}
